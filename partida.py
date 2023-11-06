bfe5775964d17982da943c0208604c4d6f577fb1
JoaoGabrielCunha <122104633+JoaoGabrielCunha@users.noreply.github.com>
2023-11-06 08:26:13 -0300

    Create partida.py

---
+++ partida.py
@@ -0,0 +1,21 @@
+def criar_baralho():
+  pass
+
+
+
+def embaralha_cartas(baralho):
+  pass
+
+
+def coloca_carta(baralho):
+  pass
+
+
+def inicia_jogo():
+  baralho= criar_baralho()
+  embaralha_cartas(baralho) 
+  coloca_carta(baralho)
+  while(True):
+    print()
+  
+    
