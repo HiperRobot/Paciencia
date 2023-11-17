0342e0a10b0c5cd19f4fe063c3a65ac9006e95ad
Thiago <thiagohnp@gmail.com>
2023-11-17 17:15:39 -0300

    MainModulada

---
+++ baralho.py
@@ -0,0 +1,41 @@
+import random
+
+def criar_baralho():
+    naipes = ['♠', '♥', '♦', '♣']
+    valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    baralho = [[valor, naipe,False] for naipe in naipes for valor in valores]
+    random.shuffle(baralho)
+    return baralho
+
+
+a= criar_baralho()
+print(a)
+
+def contaCartas(baralho):
+  print(len(baralho))
+  return len(baralho)
+
+
+
+def embaralhaCartas(baralho):
+  novobaralho= random.shuffle(baralho)
+  return novobaralho
+
+def compraCarta(tabuleiro, baralho):
+  for slot in tabuleiro:
+      
+      if not baralho:
+          print("O baralho está vazio.")
+          break
+      
+      carta = baralho.pop(0)
+      
+      slot.append(carta)
+     
+      carta[-1] = True
+
+
+
+
+
+
